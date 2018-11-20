bba1ef4ca06131a3d8c1785a0b2e8386a3a8e0db
husen233 <sen.hu@biz-united.com.cn>
2018-11-20 20:29:48 +0800

    up

--- ssaw-stream-hello/src/main/java/com/ssaw/ssawstreamhello/sink/SinkServer.java
+++ ssaw-stream-hello/src/main/java/com/ssaw/ssawstreamhello/sink/SinkServer.java
@@ -3,14 +3,17 @@ package com.ssaw.ssawstreamhello.sink;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import java.io.UnsupportedEncodingException;
 
+/**
+ * @author HuSen.
+ * @date 2018/11/20 15:52.
+ */
 @Slf4j
-@EnableBinding(Sink.class)
+@EnableBinding(value = {SinkSender.class})
 public class SinkServer {
 
-    @StreamListener(Sink.INPUT)
+    @StreamListener(SinkSender.INPUT)
     public void receive(Object payload) throws UnsupportedEncodingException {
         log.info("Received:{}", new String((byte[]) payload, "UTF-8"));
     }
