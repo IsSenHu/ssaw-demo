5673c995471fffb78c090d82f9111c1bd4632d90
husen233 <sen.hu@biz-united.com.cn>
2018-12-10 19:57:35 +0800

    add model

---
+++ ssaw-authenticate-center/ssaw-authenticate-center-service/src/main/java/com/ssaw/ssawuserresourceservice/config/WebSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.ssaw.ssawuserresourceservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author HuSen.
+ * @date 2018/11/28 10:53.
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "admin";
+
+    /**
+     * @return 使用内存用户名和密码
+     */
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername(USERNAME).password(getPasswordEncoder().encode(PASSWORD)).authorities("USER").build());
+        return manager;
+    }
+
+    /**
+     * @param auth AuthenticationManagerBuilder
+     * @throws Exception Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .and().formLogin().permitAll()
+                .and().csrf().disable();
+
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
