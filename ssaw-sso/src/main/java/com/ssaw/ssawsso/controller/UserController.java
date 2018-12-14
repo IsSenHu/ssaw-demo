49fc3ce23682ecaf425b33f29aa8569504fca1cc
husen233 <sen.hu@biz-united.com.cn>
2018-12-14 17:49:11 +0800

    change token

--- ssaw-sso/src/main/java/com/ssaw/ssawsso/controller/UserController.java
+++ ssaw-sso/src/main/java/com/ssaw/ssawsso/controller/UserController.java
@@ -2,7 +2,6 @@ package com.ssaw.ssawsso.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
 
 /**
