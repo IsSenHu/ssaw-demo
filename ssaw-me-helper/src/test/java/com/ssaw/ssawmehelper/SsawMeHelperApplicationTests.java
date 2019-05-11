f51b84ebbdc522b079b5bb35ee9a0ef065999e6e
issenhu <1178515826@qq.com>
2019-05-11 14:40:04 +0800

    test

--- ssaw-me-helper/src/test/java/com/ssaw/ssawmehelper/SsawMeHelperApplicationTests.java
+++ ssaw-me-helper/src/test/java/com/ssaw/ssawmehelper/SsawMeHelperApplicationTests.java
@@ -1,12 +1,17 @@
 package com.ssaw.ssawmehelper;
 
+import com.ssaw.redis.lock.RedisSemaphore;
 import com.ssaw.ssawmehelper.dao.redis.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Objects;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -61,5 +66,35 @@ public class SsawMeHelperApplicationTests {
     public void demo5() throws InterruptedException {
         lockDao.lock("husen");
         lockDao.unlock("husen");
+    }
+
+    @Test
+    public void demo6() throws InterruptedException {
+        RedisSemaphore semaphore = new RedisSemaphore(stringRedisTemplate, 2);
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                String acquire = semaphore.acquire("semaphore:key");
+                System.out.println(acquire);
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (Objects.nonNull(acquire)) {
+                    semaphore.release("semaphore:key", acquire);
+                }
+            }).start();
+        }
+        Thread.sleep(1000000L);
+    }
+
+    @Test
+    public void demo7() {
+        stringRedisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+            connection.zAdd("233".getBytes(), System.currentTimeMillis(), "333".getBytes());
+            Long aLong = connection.zRank("233".getBytes(), "333".getBytes());
+            List<Object> objects = connection.closePipeline();
+            return null;
+        });
     }
 }
